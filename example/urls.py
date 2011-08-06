c016f3c97932a1de04907bbd9c277b895aad5c1a
Evgeny V. Generalov <e.generalov@gmail.com>
2011-08-06 17:24:49 +0600

    fix example

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,9 @@
 import settings
 from django.conf.urls.static import static
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('example.views',
